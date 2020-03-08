829879931647928831e5ebcdf045ab90e9fa90fc
Leslie Tang <tangweejieleslie+github@gmail.com>
2020-03-08 12:28:00 +0800

    draw circle

--- src/view/components/controls/GradientPrompt.jsx
+++ src/view/components/controls/GradientPrompt.jsx
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class GradientPrompt extends Component {
-    render(){
-        return(
-            <svg height="24" viewBox="0 0 125 24" width="125"
-                xmlns="http://www.w3.org/2000/svg">
-                    <g fill="none" fill-rule="evenodd">
-                        <path d="m4.5 12h58.5 58.5" stroke="#979797" stroke-linecap="round" stroke-width="2"></path>
-                        <circle cx="63" cy="12" fill="red" r="7"></circle>
-                    </g>
-            </svg>
-        );
-    }
+  render() {
+    return (
+            <div 
+            style={{
+                "display": "flex",
+                "flex-direction": "row",
+                "justify-content": "center" 
+            }}
+            >
+                <svg height="200" width="200">
+                <circle cx="100" cy="100" r="90" stroke="black" stroke-width="3" fill="red" />
+                </svg>
+            </div>
+
+    );
+  }
 }
 
 export default GradientPrompt;
