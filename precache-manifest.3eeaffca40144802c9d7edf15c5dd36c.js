65774642180d4eb917fc0093061d33a4da73daa4
Leslie Tang <tangweejieleslie+github@gmail.com>
2020-03-09 20:55:32 +0800

    Updates

---
+++ precache-manifest.3eeaffca40144802c9d7edf15c5dd36c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155d9bfa0c9dc25b046e747f6d4264e7",
+    "url": "/react-radial-slider/index.html"
+  },
+  {
+    "revision": "bf430f364533157ab3dd",
+    "url": "/react-radial-slider/static/css/main.19dd2faf.chunk.css"
+  },
+  {
+    "revision": "a8d7d13b1a7434d1420b",
+    "url": "/react-radial-slider/static/js/2.00cabf9b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-radial-slider/static/js/2.00cabf9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf430f364533157ab3dd",
+    "url": "/react-radial-slider/static/js/main.9a6c38e0.chunk.js"
+  },
+  {
+    "revision": "0bedda61c671fdddaec7",
+    "url": "/react-radial-slider/static/js/runtime-main.d96e2d94.js"
+  }
+]);
