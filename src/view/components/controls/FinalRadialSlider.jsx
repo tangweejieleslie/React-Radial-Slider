dce8a24f7085ec8c85273d4e28c3f44c8a840031
Leslie Tang <tangweejieleslie+github@gmail.com>
2020-03-09 10:36:02 +0800

    hard coded sun position

--- src/view/components/controls/FinalRadialSlider.jsx
+++ src/view/components/controls/FinalRadialSlider.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./controls.css";
+import sun from "./sun.svg";
 
 const SVG_WIDTH = 400;
 const SVG_HEIGHT = 400;
@@ -156,7 +157,7 @@ class FinalRadialSlider extends Component {
           stroke="#f0f0f0"
           strokeWidth="14"
           transform={this.state.transform}
-        ></line>        
+        ></line>
         <line
           id="SliderButton"
           x1="200"
@@ -171,7 +172,6 @@ class FinalRadialSlider extends Component {
           onMouseDown={this.handleMouseDown}
           onMouseUp={this.handleMouseUp}
         ></line>
-
       </svg>
     );
   }
@@ -295,19 +295,46 @@ class FinalRadialSlider extends Component {
     );
   }
 
+  // https://icons8.com/icons/set/sun
+  renderSun(color) {
+
+    const logowidth = 30;
+    const logoheight = 30;
+
+    return (
+      <svg
+      fill={color}
+        // fill="#fafafa"
+        // fill="black"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width={logowidth}
+        height={logoheight}
+        x={CENTER_X-logowidth/2}
+        y={CENTER_Y-logoheight/2 + 95}
+      >
+        <path d="M 11 0 L 11 3 L 13 3 L 13 0 L 11 0 z M 4.2226562 2.8085938 L 2.8085938 4.2226562 L 4.9296875 6.34375 L 6.34375 4.9296875 L 4.2226562 2.8085938 z M 19.777344 2.8085938 L 17.65625 4.9296875 L 19.070312 6.34375 L 21.191406 4.2226562 L 19.777344 2.8085938 z M 12 5 C 8.1458514 5 5 8.1458514 5 12 C 5 15.854149 8.1458514 19 12 19 C 15.854149 19 19 15.854149 19 12 C 19 8.1458514 15.854149 5 12 5 z M 12 7 C 14.773268 7 17 9.2267316 17 12 C 17 14.773268 14.773268 17 12 17 C 9.2267316 17 7 14.773268 7 12 C 7 9.2267316 9.2267316 7 12 7 z M 0 11 L 0 13 L 3 13 L 3 11 L 0 11 z M 21 11 L 21 13 L 24 13 L 24 11 L 21 11 z M 4.9296875 17.65625 L 2.8085938 19.777344 L 4.2226562 21.191406 L 6.34375 19.070312 L 4.9296875 17.65625 z M 19.070312 17.65625 L 17.65625 19.070312 L 19.777344 21.191406 L 21.191406 19.777344 L 19.070312 17.65625 z M 11 21 L 11 24 L 13 24 L 13 21 L 11 21 z" />
+      </svg>
+    );
+  }
+
   render() {
     return (
       <div>
         <h1> Slider </h1>
+        <div class="flexbox">
         <svg height={SVG_HEIGHT} width={SVG_WIDTH} id="SVG_BOX">
           {this.renderBackground()}
           {this.renderSliderArea()}
           {this.renderSliderPrompt()}
           {this.renderSlider()}
-          
+
           {this.renderTargetedTemperature(50)}
           {this.renderCurrentTemperature(10)}
+          {this.renderSun("pink")}
         </svg>
+        </div>
+
       </div>
     );
   }
